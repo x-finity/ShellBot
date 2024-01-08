1d544cd60e323485f2af009dc53d74807f6c52bb
XFinity <x.finity777@gmail.com>
2024-01-08 16:06:10 +0300

    default conf and log

---
+++ config.py
@@ -0,0 +1,4 @@
+# Change to your bot token of @BotFather
+TOKEN = '<ENTER YOUR TOKEN>'
+# Change to the id number(s) of the user(s) who is/are authorized to use the bot (@userinfobot)
+authorizedUsers = [<ENTER USER TGID>]
