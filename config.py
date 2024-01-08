2435e77a9d536bb3881e13ae187840c0ec104158
XFinity <x.finity777@gmail.com>
2024-01-08 15:48:57 +0300

    replace 'exec' with 'shell' command

--- config.py
+++
@@ -1,4 +0,0 @@
-# Change to your bot token of @BotFather
-TOKEN = '<ENTER YOUR TOKEN>'
-# Change to the id number(s) of the user(s) who is/are authorized to use the bot (@userinfobot)
-authorizedUsers = [<ENTER USER TGID>]
